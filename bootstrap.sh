4b9748944a556a075a1038537c8e76bfd41dc0b6
Stefan Sundin <git@stefansundin.com>
2016-05-21 23:38:26 -0700

    Test keys.

---
+++ bootstrap.sh
@@ -0,0 +1,2 @@
+#!/bin/bash -ex
+go get github.com/andrew-d/go-termutil
--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
 
-import "os"
-import "io"
-import "bufio"
-import "fmt"
-import "regexp"
+import (
+  "os"
+  "flag"
+  "io"
+  "io/ioutil"
+  "bufio"
+  "fmt"
+  "regexp"
+  "net/http"
+  "github.com/andrew-d/go-termutil"
+)
 
 type Pattern struct {
   provider string
@@ -21,11 +27,32 @@ func append_unique(slice []string, add string) []string {
   return append(slice, add)
 }
 
+func get_url(url string) []byte {
+  resp, err := http.Get(url)
+  if err != nil {
+    panic(err)
+  }
+  data, err := ioutil.ReadAll(resp.Body)
+  if err != nil {
+    panic(err)
+  }
+  return data
+}
+
 func main() {
+  test_flag := flag.Bool("test", false, "Test API keys to see if they are still active")
+  flag.Parse()
+
+  if termutil.Isatty(os.Stdin.Fd()) {
+    fmt.Println("Usage: git log -p | ./secrets-scanner")
+    fmt.Println("Use -test to test found API keys.")
+    return
+  }
+
   scanners := []Pattern {
     {
       "google_access_token",
-      regexp.MustCompile("ya29.[0-9a-zA-Z]{68}"),
+      regexp.MustCompile("ya29.[0-9a-zA-Z_\\-]{68}"),
       make([]string, 0),
     },
   }
@@ -45,16 +72,20 @@ func main() {
   }
 
   for _, v := range scanners {
+    if len(v.matches) == 0 {
+      continue
+    }
     if v.provider == "google_access_token" {
       fmt.Println("Found Google access tokens:")
       for _, m := range v.matches {
-        url := "https://www.googleapis.com/oauth2/v1/tokeninfo?access_token="+m
-        fmt.Printf("- %s => %s\n", m, url)
+        fmt.Printf("- %s\n", m)
+        if *test_flag {
+          url := "https://www.googleapis.com/oauth2/v1/tokeninfo?access_token="+m
+          fmt.Printf("%s\n%s\n", url, get_url(url))
+        }
       }
     } else {
       fmt.Printf("%s -> %s\n", v.provider, v.matches)
     }
   }
-
-  fmt.Println("Goodbye!")
 }
